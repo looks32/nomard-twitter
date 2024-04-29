55230cfb9032027537d68a32464701435acd3908
Dang <56210432+looks32@users.noreply.github.com>
2024-04-29 11:31:01 +0900

    Update edit.tsx

--- pages/tweets/[id]/edit.tsx
+++ pages/tweets/[id]/edit.tsx
@@ -1,5 +1,5 @@
 import { useForm } from 'react-hook-form';
-import Input from '../../../components/input';
+//import Input from '../../../components/input';
 import Layout from '../../../components/layout';
 import TextArea from '../../../components/textarea';
 import Button from '../../../components/button';
